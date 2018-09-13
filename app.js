39da00838d94374dd064776f3c2b095454334d58
Perzival1312 <jj.mongoose11@hotmail.com>
2018-09-13 10:24:21 -0700

    added tests

--- app.js
+++ app.js
@@ -47,3 +47,55 @@ app.delete('/reviews/comments/:id', function (req, res) {
     console.log(err.message);
   })
 })
+
+app.get('/', (req, res) => {
+  Review.find().then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    }).catch(err => {
+      console.log(err);
+    })
+});
+// CREATE
+app.post('/reviews', (req,res) => {
+  Review.create(req.body).then((review) =>{
+    console.log(review);
+    res.redirect(`/reviews/${review._id}`);
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// EDIT
+  app.get('/reviews/:id/edit', function (req,res) {
+    Review.findById(req.params.id, function(err, review){
+      res.render('reviews-edit', {review: review});
+    })
+  })
+// NEW
+  // app.get('/reviews/new', (req,res) => {
+  //   res.render('reviews-new', {});
+  // });
+// UPDATE
+  app.put('/reviews/:id', (req,res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    }).catch(err => {
+      console.log(err.message)
+    })
+  })
+// DESTROY
+  app.delete('/reviews/:id', function (req,res) {
+    console.log("DELETE review");
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
+
+  // app.listen(3000, () => {
+  //   console.log('App listening on port 3000!')
+  // });
+
+  module.exports = app;
