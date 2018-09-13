c9f223dad6d18a74720fc060bde003e9793ff766
Perzival1312 <jj.mongoose11@hotmail.com>
2018-09-12 18:53:17 -0700

    users can edit reviews

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express()
 
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true}));
-// app.get('/', (req, res) => {
-//   res.render('home', {msg: 'Hello World!'});
-// })
-// let reviews = [
-//   { title: "Great Review" },
-//   { title: "Next Review" },
-//   { title: "This is dull" }
-// ]
+app.use(methodOverride('_method'));
+
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
@@ -24,10 +19,10 @@ const Review = mongoose.model('Review', {
   rating: Number
 });
 
-app.post('/reviews', (req,res) => {
+app.put('/reviews', (req,res) => {
   Review.create(req.body).then((review) =>{
-    // console.log(review);
-    res.redirect('/');
+    console.log(review);
+    res.redirect(`/reviews/${review._id}`);
   }).catch((err) => {
     console.log(err.message);
   })
@@ -46,15 +41,31 @@ app.get('/reviews/:id', (req,res) => {
   Review.findById(req.params.id).then((review) => {
     res.render('reviews-show', {review: review})
   }).catch((err) => {
-      console.log("/rviews/:id")
+      console.log("/reviews/:id")
       console.log(err.message);
   })
 });
+
+app.get('/reviews/:id/edit', function (req,res) {
+  Review.findById(req.params.id, function(err, review){
+    res.render('reviews-edit', {review: review});
+  })
+})
 
 app.get('/reviews/new', (req,res) => {
   res.render('reviews-new', {});
   console.log(err)
 });
+
+// update
+app.put('/reviews/:id', (req,res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+  .then(review => {
+    res.redirect(`/reviews/${review._id}`)
+  }).catch(err => {
+    console.log(err.message)
+  })
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
