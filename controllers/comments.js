ee63405b66222d13424eecb6c4057a827c122125
Perzival1312 <jj.mongoose11@hotmail.com>
2018-11-08 10:25:38 -0800

    adding ajax

--- controllers/comments.js
+++ controllers/comments.js
@@ -9,12 +9,21 @@ module.exports = function (app, Comment) {
     })
   })
   // NEW Comment
-  app.post('/reviews/comments', (req, res) => {
-    console.log("recieved comment")
-    Comment.create(req.body).then(comment => {
-      res.redirect(`/reviews/${comment.reviewId}`);
-    }).catch((err) => {
-      console.log(err.message);
-    });
-  });
+  // app.post('/reviews/comments', (req, res) => {
+  //   console.log("recieved comment")
+  //   Comment.create(req.body).then(comment => {
+  //     res.redirect(`/reviews/${comment.reviewId}`);
+  //   }).catch((err) => {
+  //     console.log(err.message);
+  //   });
+  // });
+
+  // CREATE Comment
+app.post('/reviews/comments', (req, res) => {
+  Comment.create(req.body).then(comment => {
+    res.status(200).send({ comment: comment });
+  }).catch((err) => {
+    res.status(400).send({ err: err })
+  })
+})
 }
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,17 +1,68 @@
-// alert("hello world");
-const axios = require('axios');
-console.log("hello")
-
-// Make a request to the color api
 axios.get('http://www.thecolorapi.com/id?hex=24B1E0')
-// alert("Hello World!")
   .then(function (response) {
-    // handle success
-    // alert("Hello World!")
-    console.log("hello")
-    alert(response.hex.clean);
-  })
-  .catch(function (error) {
-    // handle error
+    console.log(response.data.hex.clean)
+  }).catch(function (error) {
+    if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+
+    } else if (error.request) {
+      alert(error.request);
+    } else {
+      alert('Error', error.message);
+    }
+    alert(error.config);
+  }).catch(function (error) {
     console.log(error);
-  });
+});
+
+document.getElementById("new-comment").addEventListener("submit", e => {
+    // prevent the default form behavior
+    e.preventDefault();
+
+    // serialize the form data into an object
+    let comment = {};
+    const inputs = document.getElementsByClassName('form-control');
+    for (var i = 0; i < inputs.length; i++) {
+      comment[inputs[i].name] = inputs[i].value;
+    }
+
+    // use axios to initialize a post request and send in the form data
+
+    axios.post('/reviews/comments', comment)
+      .then(function (response) {
+        // wait for the success response from the server
+        console.log(response);
+        // remove the information from the form
+        // display the data as a new comment on the page
+      })
+      .catch(function (error) {
+        console.log(error);
+        // handle any errors
+        alert('There was a problem saving your comment. Please try again.')
+      });
+});
+
+axios.post('/user', comment)
+     .then(function (response) {
+       // wait for the success response from the server
+       console.log(response);
+       // remove the information from the form
+       this.reset();
+       // display the data as a new comment on the page
+       document.getElementById('comments').prepend(
+         `
+          <div class="card">
+            <div class="card-block">
+              <h4 class="card-title">${response.title}</h4>
+              <p class="card-text">${response.content}</p>
+              <p>
+                 <form method="POST" action="/reviews/comments/${response._id}?_method=DELETE">
+                   <button class="btn btn-link" type="submit">Delete</button>
+                 </form>
+              </p>
+            </div>
+          </div>
+         `
+       );
+     })
